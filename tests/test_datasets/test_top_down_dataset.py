b894485c1a61d8060e3cb32f81332adfe72eab56
jin-s13 <jinsheng13@foxmail.com>
2020-12-28 16:56:11 +0800

    add tests for 300w

--- tests/test_datasets/test_top_down_dataset.py
+++ tests/test_datasets/test_top_down_dataset.py
@@ -956,3 +956,55 @@ def test_top_down_JHMDB_dataset():
 
         with pytest.raises(KeyError):
             _ = custom_dataset.evaluate(outputs, tmpdir, 'mAP')
+
+
+def test_face_300W_dataset():
+    dataset = 'Face300WDataset'
+    # test Face 300W datasets
+    dataset_class = DATASETS.get(dataset)
+    dataset_class.load_annotations = MagicMock()
+    dataset_class.coco = MagicMock()
+
+    channel_cfg = dict(
+        num_output_channels=68,
+        dataset_joints=68,
+        dataset_channel=[
+            list(range(68)),
+        ],
+        inference_channel=list(range(68)))
+
+    data_cfg = dict(
+        image_size=[256, 256],
+        heatmap_size=[64, 64],
+        num_output_channels=channel_cfg['num_output_channels'],
+        num_joints=channel_cfg['dataset_joints'],
+        dataset_channel=channel_cfg['dataset_channel'],
+        inference_channel=channel_cfg['inference_channel'])
+    # Test
+    data_cfg_copy = copy.deepcopy(data_cfg)
+    _ = dataset_class(
+        ann_file='tests/data/300w/test_300w.json',
+        img_prefix='tests/data/300w/',
+        data_cfg=data_cfg_copy,
+        pipeline=[],
+        test_mode=True)
+
+    custom_dataset = dataset_class(
+        ann_file='tests/data/300w/test_300w.json',
+        img_prefix='tests/data/300w/',
+        data_cfg=data_cfg_copy,
+        pipeline=[],
+        test_mode=False)
+
+    assert custom_dataset.test_mode is False
+    assert custom_dataset.num_images == 2
+    _ = custom_dataset[0]
+
+    outputs = load_json_to_output('tests/data/300w/test_300w.json',
+                                  'tests/data/300w/')
+    with tempfile.TemporaryDirectory() as tmpdir:
+        infos = custom_dataset.evaluate(outputs, tmpdir, ['NME'])
+        assert_almost_equal(infos['NME'], 0.0)
+
+        with pytest.raises(KeyError):
+            _ = custom_dataset.evaluate(outputs, tmpdir, 'mAP')
